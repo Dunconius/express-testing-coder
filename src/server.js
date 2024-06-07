064440d1968c020ab8c8fe9ddc06ccca2d10369a
Dunconius <duncanbean84@gmail.com>
2024-06-08 09:53:00 +1000

    feat: api versioning

--- src/server.js
+++ src/server.js
@@ -2,26 +2,22 @@
 const express = require("express");
 const app = express();
 
+
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (request, response, next) => {
-    
-    return next(new Error("Error on purpose from root route"))
-
-    response.json({
-        message:"Hello World!"
-    });
-});
+const apiV2 = require("./v2/index.js");
+app.use("/v2", apiV2);
 
 app.use((error, request, response, next) => {
-    response.json({
-        message:"Error occured",
-        error: error
-    });
+	response.json({
+		message:"Error occured!",
+		error: error
+	});
 });
 
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+	console.log(`Server is running on port ${port}`);
+});
